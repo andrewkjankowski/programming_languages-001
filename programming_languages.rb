256751d212aeb28de55e2e5f19a9aa1de0dc8c9e
andrewkjankowski <andrewkjankowski@gmail.com>
2018-01-15 16:08:59 -0500

    Done.

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,3 +1,18 @@
 def reformat_languages(languages)
   # your code here
+  reformed = {}
+  languages.each do |style, languages|
+    languages.each do |language, attributes|
+      reformed[language] = {:type => "", :style => []}
+    end
+  end
+
+  languages.each do |style, languages|
+    languages.each do |language, attributes|
+      reformed[language][:type] = attributes[:type]
+      reformed[language][:style] << style
+    end
+  end
+
+  reformed
 end
